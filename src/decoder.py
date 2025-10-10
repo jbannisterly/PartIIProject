a1bd96c5a324cd77a51aba107bc14eddd0a9ad38
jbannisterly <jbannisterly@gmail.com>
2025-10-10 22:31:30 +0100

    Decoder

---
+++ src/decoder.py
@@ -0,0 +1,14 @@
+import bitarray as ba
+import cv2
+import numpy as np
+
+image = cv2.imread("../output/output.png")
+pixels = image.reshape(25 * 25, 3)
+bitArray = [1 if pixel[0] > 127 else 0 for pixel in pixels]
+lengthArray = np.packbits(bitArray[0:16])
+length = int(lengthArray[0]) + int(lengthArray[1]) * 256  
+bytes = bytearray(np.packbits(bitArray[16:length + 16]))
+
+f = open("../output/output.txt", "w+")
+f.write(bytes.decode())
+f.close()
