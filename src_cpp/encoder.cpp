78359ab2567cdbcdd771f6169457749857d09feb
jbannisterly <jbannisterly@gmail.com>
2025-10-28 22:14:31 +0000

    Changed encoder structure

--- src_cpp/encoder.cpp
+++ src_cpp/encoder.cpp
@@ -5,8 +5,7 @@
 
 using namespace cv;
 
-int main(){
-    char* message = "Hello World\n";
+uint8_t* EncodeMessage(char* message){
     uint16_t length = strlen(message);
 
     uint8_t* byteData = new uint8_t[length + 2];
@@ -14,21 +13,80 @@ int main(){
     ((uint16_t*)byteData)[0] = length << 3; // convert to number of bits
     std::memcpy(byteData + 2, message, length);
 
-    uint8_t* rawData = new uint8_t[25 * 25 * 3];
+    return byteData;
+}
 
-    for (int i = 0; i < length + 2; i++){
+struct PixelData{
+    uint8_t* data;
+    int length;
+};
+
+PixelData* MessageToPixels(uint8_t* byteData, uint16_t length){
+    PixelData* pixelData = new PixelData();
+
+    pixelData->data = (uint8_t*)malloc(length * 24 * sizeof(uint8_t));
+    pixelData->length = length * 24;
+
+    for (int i = 0; i < length; i++){
         uint8_t currentByte = byteData[i];
         for (int j = 0; j < 8; j++){
             uint8_t data = (currentByte & 128) ? 255 : 0;
             currentByte <<= 1;
             for (int k = 0; k < 3; k++){
-                rawData[i * 24 + j * 3 + k] = data;
+                pixelData->data[i * 24 + j * 3 + k] = data;
             }
         }
     }
 
-    Mat image (25, 25, CV_8UC3);
-    image.data = rawData;
+    return pixelData;
+}
+
+struct BarcodeLayout{
+    uint8_t* data;
+    uint8_t* mask;
+};
+
+BarcodeLayout* GetBarcode(){
+    BarcodeLayout* barcode = new BarcodeLayout();
+    int size = 25 * 25;
+    barcode->data = (uint8_t*)malloc(sizeof(uint8_t) * size * 3);
+    barcode->mask = (uint8_t*)malloc(sizeof(uint8_t) * size);
+    memset(barcode->data, 0, size * 3);
+    memset(barcode->mask, 255, size);
+    return barcode;
+}
+
+uint8_t* PixelsToBarcode(PixelData* pixels){
+    int size = 25 * 25 * 3;
+    BarcodeLayout* barcode = GetBarcode();
+
+    int sourceCounter = 0;
+    int targetCounter = 0;
+
+    while(sourceCounter < pixels->length){
+        if (barcode->mask[targetCounter] > 0){
+            barcode->data[targetCounter] = pixels->data[sourceCounter];
+            sourceCounter++;
+        }
+        targetCounter++;
+    }
+
+    free(barcode->mask);
+
+    return barcode->data;
+}
+
+
+
+int main(){
+    char* message = "Hello World\n";
+
+    uint8_t* byteData = EncodeMessage(message);
+    PixelData* pixelData = MessageToPixels(byteData, strlen(message) + 2);
+    uint8_t* imageData = PixelsToBarcode(pixelData);
+
+    Mat image(25, 25, CV_8UC3);
+    image.data = imageData;
 
     imwrite("output/output.png", image);
 }
