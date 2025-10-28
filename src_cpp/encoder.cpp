619da4039388bb30afa9e544f8290acb8cbac6e1
jbannisterly <jbannisterly@gmail.com>
2025-10-28 22:44:38 +0000

    added  finder pattern

--- src_cpp/encoder.cpp
+++ src_cpp/encoder.cpp
@@ -46,13 +46,32 @@ struct BarcodeLayout{
     uint8_t* mask;
 };
 
+void DrawSquare(BarcodeLayout barcode, uint8_t colour, int positionX, int positionY, int barcodeWidth, int squareSize){
+    for (int i = 0; i < squareSize; i++){
+        int offset = ((i + positionY) * barcodeWidth + positionX);
+        memset(barcode.data + offset * 3, colour, squareSize * 3);
+        memset(barcode.mask + offset, 0, squareSize);
+    }
+}
+
 BarcodeLayout* GetBarcode(){
     BarcodeLayout* barcode = new BarcodeLayout();
     int size = 25 * 25;
     barcode->data = (uint8_t*)malloc(sizeof(uint8_t) * size * 3);
     barcode->mask = (uint8_t*)malloc(sizeof(uint8_t) * size);
+
     memset(barcode->data, 0, size * 3);
     memset(barcode->mask, 255, size);
+
+    int posX[3] = {0, 17, 0};
+    int posY[3] = {0, 17, 17};
+
+    for (int i = 0; i < 3; i++){
+        DrawSquare(*barcode, 0, posX[i], posY[i], 25, 7);
+        DrawSquare(*barcode, 255, posX[i] + 1, posY[i] + 1, 25, 5);
+        DrawSquare(*barcode, 0, posX[i] + 2, posY[i] + 2, 25, 3);
+    }
+
     return barcode;
 }
 
@@ -65,8 +84,8 @@ uint8_t* PixelsToBarcode(PixelData* pixels){
 
     while(sourceCounter < pixels->length){
         if (barcode->mask[targetCounter] > 0){
-            barcode->data[targetCounter] = pixels->data[sourceCounter];
-            sourceCounter++;
+            memcpy(barcode->data + targetCounter * 3, pixels->data + sourceCounter, 3);
+            sourceCounter += 3;
         }
         targetCounter++;
     }
