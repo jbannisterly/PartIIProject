98d765c04648f1aed0060f710e42c407f0d58737
jbannisterly <jbannisterly@gmail.com>
2025-12-05 00:44:38 +0000

    reed solomon decoder

--- src_cpp/error_correction_schifra.cpp
+++ src_cpp/error_correction_schifra.cpp
@@ -24,19 +24,45 @@ std::vector<uint8_t> Encode(std::vector<uint8_t> rawData){
 
     for (int i = 0; i < nBlocks; i++){
         if (i == nBlocks - 1){
-            inData = std::string((char*)rawData.data(), rawData.size() % 223);
+            inData = std::string((char*)rawData.data() + i * 223, rawData.size() % 223);
             inData.resize(223, 0);
         }else{
-            inData = std::string((char*)rawData.data(), 223);
+            inData = std::string((char*)rawData.data() + i * 223, 223);
         }
         encoder.encode(inData, block);
 
-        for (int j = 0; j < 256; j++){
+        for (int j = 0; j < 255; j++){
             encodedData.push_back(block.data[j]);
         }
     }
 
     return encodedData;
+}
+
+std::vector<uint8_t> Decode(std::vector<uint8_t> encodedData){
+    int nBlocks = encodedData.size() / 255;
+
+    std::vector<uint8_t> decodedData;
+
+    schifra::galois::field field(8, schifra::galois::primitive_polynomial_size06, schifra::galois::primitive_polynomial06);
+    schifra::reed_solomon::decoder<255, 32, 223> decoder(field, 120);
+
+    schifra::reed_solomon::block<255, 32> block;
+
+    for (int i = 0; i < nBlocks; i++){
+        std::string data((char*)encodedData.data() + i * 255, 223);
+        std::string fec((char*)encodedData.data() + i * 255 + 223, 32); 
+
+        block = schifra::reed_solomon::block<255, 32>(data, fec);
+
+        decoder.decode(block);
+
+        for (int j = 0; j < 223; j++){
+            decodedData.push_back(block.data[j]);
+        }
+    }
+
+    return decodedData;
 }
 
 int main(){
@@ -48,8 +74,16 @@ int main(){
 
     std::vector<uint8_t> output = Encode(input);
 
-    std::cout << output.size() << std::endl;
-    for (int i = 0; i < output.size(); i++) std::cout << (int)output[i] << " ";
+    for (int i = 0; i < 10; i++){ // noise
+        output[i] = 88;
+    }
+
+    std::vector<uint8_t> decoded = Decode(output);
+
+    std::cout << decoded.size() << std::endl;
+    for (int i = 0; i < input.size(); i++) {
+        if (input[i] != decoded[i]) std::cout << "fail";
+    }
 
     return 0;
 }
