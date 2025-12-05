54629ae9bd488619e8fc97c6539056ba40e9b43a
jbannisterly <jbannisterly@gmail.com>
2025-12-05 00:27:47 +0000

    add error correction bytes

--- src_cpp/error_correction_schifra.cpp
+++ src_cpp/error_correction_schifra.cpp
@@ -6,26 +6,50 @@
 #include <schifra/schifra_reed_solomon_block.hpp>
 #include <schifra/schifra_error_processes.hpp>
 #include <iostream>
+#include <stdint.h>
+#include <cmath> 
 
+std::vector<uint8_t> Encode(std::vector<uint8_t> rawData){
+    int nBlocks = std::ceil((float)rawData.size() / 223);
 
+    std::vector<uint8_t> encodedData;
 
-int main(){
     schifra::galois::field field(8, schifra::galois::primitive_polynomial_size06, schifra::galois::primitive_polynomial06);
-    
     schifra::galois::field_polynomial genPoly(field);
     schifra::make_sequential_root_generator_polynomial(field, 120, 32, genPoly);
     schifra::reed_solomon::encoder<255, 32, 223> encoder(field, genPoly);
 
-    std::string message = "Hello world. Lorem Ipsum Dolor Sit Amet.";
-
-    message.resize(255, 0);
-
     schifra::reed_solomon::block<255, 32> block;
-    encoder.encode(message, block);
+    std::string inData;
 
-    for (int i = 0; i < 256; i++){
-        std::cout << block.data[i] << " ";
+    for (int i = 0; i < nBlocks; i++){
+        if (i == nBlocks - 1){
+            inData = std::string((char*)rawData.data(), rawData.size() % 223);
+            inData.resize(223, 0);
+        }else{
+            inData = std::string((char*)rawData.data(), 223);
+        }
+        encoder.encode(inData, block);
+
+        for (int j = 0; j < 256; j++){
+            encodedData.push_back(block.data[j]);
+        }
     }
+
+    return encodedData;
+}
+
+int main(){
+    std::vector<uint8_t> input;
+
+    for (int i = 0; i < 1000; i++) {
+        input.push_back((i * 3) % 78);
+    }
+
+    std::vector<uint8_t> output = Encode(input);
+
+    std::cout << output.size() << std::endl;
+    for (int i = 0; i < output.size(); i++) std::cout << (int)output[i] << " ";
 
     return 0;
 }
