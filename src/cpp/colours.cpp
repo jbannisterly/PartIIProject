b9d68efcab82045f55f5cc0744f51d779ff0b802
jbannisterly <jbannisterly@gmail.com>
2026-01-03 21:04:05 +0000

    function prototypes for colours

---
+++ src/cpp/colours.cpp
@@ -0,0 +1,9 @@
+#include "colours.hpp"
+
+std::vector<uint8_t> ColourPixels::DataToPixels(std::vector<uint8_t> data) {
+    return std::vector<uint8_t>();
+}
+
+std::vector<uint8_t> ColourPixels::PixelsToData(std::vector<uint8_t> pixels) {
+    return std::vector<uint8_t>();
+}
---
+++ src/cpp/colours.hpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+struct Colour {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+
+    Colour(uint8_t inR, uint8_t inG, uint8_t inB) :
+    r {inR} ,
+    g {inG} ,
+    b {inB} { }
+}
+
+struct ColourScheme {
+    std::vector<Colour> colours;
+
+    ColourScheme(std::vector<Colour> &inColours) :
+    colours {inColours} { }
+}
+
+class ColourPixels {
+    private:
+
+    ColourScheme colourScheme;
+
+    public:
+
+    std::vector<uint8_t> DataToPixels(std::vector<uint8_t> data);
+    std::vector<uint8_t> PixelsToData(std::vector<uint8_t> pixels);
+
+    ColourPixels (ColourScheme inColourScheme) :
+    colourScheme {inColourScheme} { }
+}
