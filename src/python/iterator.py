99538dbc5aeb50c6883b45ce81095453def3f3b4
jbannisterly <jbannisterly@gmail.com>
2026-01-23 21:20:00 +0000

    python optimiser

--- src/python/iterator.py
+++ src/python/iterator.py
@@ -1,11 +1,20 @@
 import subprocess
+import numpy as np
+from scipy.optimize import minimize
+
+def MakeGuess(bounds):
+  bound_str = [str(x) for x in bounds]
+  error_count = str(subprocess.check_output(['./output/bin/Lib_Count'] + bound_str + ['100', '100'])).split('\\n')
+  n_errors = error_count[-2]
+  print(bounds)
+  print(n_errors)
+  return float(n_errors)
 
 initial_guess = str(subprocess.check_output(['./output/bin/Lib_Bounds'])).split("\\n")
 initial_bounds = initial_guess[-11:-3]
 
 print(initial_bounds)
 
-error_count = str(subprocess.check_output(['./output/bin/Lib_Count'] + initial_bounds + ['100', '100'])).split('\\n')
-n_errors = error_count[-2]
+methods = ['Nelder-Mead', 'Powell', 'COBYLA', 'COBYQA']
 
-print(n_errors)
+minimize(MakeGuess, initial_bounds, method=methods[0])
