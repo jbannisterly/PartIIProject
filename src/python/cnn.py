b98f0a06581feb6eb620ea6ac3d55cb4b30d2bd3
jbannisterly <jbannisterly@gmail.com>
2026-01-30 18:29:42 +0000

    generate test data

--- src/python/cnn.py
+++ src/python/cnn.py
@@ -1,6 +1,45 @@
 import torch
 import cv2
 import numpy as np
+import noise
+import subprocess
+
+class Distorter():
+    def distort(path, target):
+        barcode = cv2.imread("output/img/output.png")
+        y,x,c = barcode.shape
+        barcode = cv2.resize(barcode, (x * 8, y * 8), interpolation=cv2.INTER_NEAREST)
+        y,x,c = barcode.shape
+
+        proj_from = np.float32([[0,0],[x,0],[0,y],[x,y]])
+        proj_to = np.float32(target)
+
+        transform_matrix = cv2.getPerspectiveTransform(proj_from, proj_to)
+        barcode_proj = np.float32(cv2.warpPerspective(barcode, transform_matrix, (1080, 1920))) / 256
+
+        mask = np.zeros((1920, 1080, 3))
+        mask = cv2.fillPoly(mask, pts=[np.int32(proj_to[[0,1,3,2]])], color=(1,1,1))
+
+        background = np.ones((1920, 1080, 3)) * 0.5
+        background[:][:][2] = 0.5
+        background[:][:][1] = 0.5
+        for i in range(len(background)):
+            for j in range(len(background[i])):
+                background[i][j][0] = (noise.snoise2(i / 200, j / 200) * 0.5 + 0.5) * 256
+
+        background = np.float32(cv2.cvtColor(np.float32(background), cv2.COLOR_HSV2RGB))
+
+        barcode_background = barcode_proj * mask + background * (1-mask)
+
+        for i in range(len(barcode_background)):
+            for j in range(len(barcode_background[i])):
+                barcode_background[i][j][0] += (noise.snoise3(i * 2, j * 2, 0) * 0.1)
+                barcode_background[i][j][1] += (noise.snoise3(i * 2, j * 2, 1) * 0.1)
+                barcode_background[i][j][2] += (noise.snoise3(i * 2, j * 2, 2) * 0.1)
+
+        distorted = np.uint8(np.clip(barcode_background, 0, 255/256) * 256)
+        return distorted
+
 
 class CNN(torch.nn.Module):
     def __init__(self):
@@ -22,7 +61,13 @@ class CNN(torch.nn.Module):
 
     def forward(self, x):
         x = self.net(x)
+        print(x)
         return x
+    
+def GenImage(ids):
+    for id in ids:
+        randomData = str(np.random.random(1000))
+        subprocess.Popen(['./output/bin/Encoder', randomData[:9000], 'output/img/cnn/img_raw_' + str(id) + '.png'])
 
 def GetImage(id):
     data = cv2.imread('output/img/cnn/img_' + str(id) + '.png')
@@ -50,6 +95,8 @@ def train(model, optimiser, lossFn):
 model = CNN().to('cpu')
 optimiser = torch.optim.SGD(model.parameters(), 0.0001)
 lossFn = torch.nn.MSELoss()
+
+GenImage(np.arange(10))
 
 for i in range(100):
     train(model, optimiser, lossFn)
